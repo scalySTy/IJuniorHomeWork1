c4190cad93b8abf2fe137b38d0812b1bd27011b2
scalySTy <scalySTy@ya.ru>
2024-10-12 16:48:42 +0300

    Removing an unnecessary component

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SphereMover : MonoBehaviour
+public class Mover : MonoBehaviour
 {
     [SerializeField] private Vector3 _movementDirection;
     [SerializeField] private float _movementSpeed = 5f;
